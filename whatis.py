c91ad211cb2819986fc9b547a353932c0d96cbe1
Ananya Raval <ananya.raval@gmail.com>
2017-08-12 01:55:19 -0400

    7 Commands

--- whatis.py
+++ whatis.py
@@ -3,15 +3,30 @@ import pandas as pd
 whatis = pd.DataFrame(columns = ['Command','NL Queries'])
 
 whatis = whatis.append({'Command':'whatis head','NL Queries':['How do I know what the command head does?',
-											'Get one line description of head command.',
-											'How do I see what head command in linux does?']},ignore_index=True)
+															'Get one line description of head command.',
+															'How do I see what head command in linux does?']},ignore_index=True)
 
 whatis = whatis.append({'Command':'whatis -s "2" open','NL Queries':['Show section 2 of documentation of command open.',
-													'How do I see section 2 of man pages of what command open does?']},ignore_index=True)
+																	'How do I see section 2 of man pages of what command open does?']},ignore_index=True)
+
+whatis = whatis.append({'Command':'whatis -d ls','NL Queries':['Print debugging information of ls commands.',
+																'How do I see debugging options of ls command?']},ignore_index=True)
+
+whatis = whatis.append({'Command':'whatis -r ls','NL Queries':['Print all commands with 1 line descriptions which has ls in their names.',
+															'How do I see 1 line Descriptions of all comands which have ls in their names?']},ignore_index=True)
+
+whatis = whatis.append({'Command':'whatis -V','NL Queries':['Print the version of my whatis command.',
+															'How do I see Version of my whatis command?']},ignore_index=True)
+
+whatis = whatis.append({'Command':'whatis -l head','NL Queries':['How do I know what the command head does without trimming output?',
+																'Get one line description of head command without trimming output.',
+																'How do I see what head command in linux does without trimming output?']},ignore_index=True)
+
+whatis = whatis.append({'Command':'whatis head tail','NL Queries':['How do I know what the commands head and tail does in one line?',
+																'Get one line description of head & tail commands.',
+																'How do I see what head & tail commands in linux do?']},ignore_index=True)
 
 '''
-whatis = whatis.append({'Command':'whatis head tail','NL Queries':['How do I know what the commands head and tail does in one line?','Get one line description of head & tail commands.','How do I see what head & tail commands in linux does?']},ignore_index=True)
-
 whatis = whatis.append({'Command':'whatis whatis','NL Queries':['How do I know what the command whatis does?','Get one line description of whatis command.','How do I see what whatis command in linux does?']},ignore_index=True)
 
 whatis = whatis.append({'Command':'whatis passwd | head -n 1','NL Queries':['How do I know what the top command of passwd does?','Get exactly top one line description of passwd command.','How do I see what passwd command in linux does in exactly top one line?']},ignore_index=True)
