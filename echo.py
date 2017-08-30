de1f93954f965b78c498f969d44bdc670000c1ad
Ananya Raval <ananya.raval@gmail.com>
2017-08-30 04:32:04 -0400

    Added 8 commands

--- echo.py
+++ echo.py
@@ -2,9 +2,38 @@ import pandas as pd
 
 echo = pd.DataFrame(columns = ['Command','NL Queries'])
 
-echo = echo.append({'Command':' echo My name is Ananya','NL Queries':['Print My name is Ananya','Output this string: \'My name is Ananya.\'','How do I print My name is Ananya in command line?']},ignore_index = True)
-echo = echo.append({'Command':'echo -e Column1 \t Column2','NL Queries':['Print Column1 Column2 with a tab in between them.' 'How do I print tab separated \'Column1 Column2\'?']},ignore_index =  True)
-echo = echo.append({'Command':'echo $X','NL Queries':['Print value of variable x.','What is the value of shell variable x?','How do I output the value of variable x?']},ignore_index = True)
-echo = echo.append({'Command':'echo *','NL Queries':['Print all file names in current folder.','What files are present in this folder?','How do I find out the files present in this folder?','List all files in this folder.']},ignore_index = True)
+echo = echo.append({'Command':' echo My name is Ananya','NL Queries':['Print My name is Ananya',
+																	'Output this string: \'My name is Ananya.\'',
+																	'How do I print My name is Ananya in command line?']},ignore_index = True)
 
-print echo
+echo = echo.append({'Command':'echo -e \'Column1 \t Column2\'','NL Queries':['Enable interpretation of backslash escape sequences and print \'Column1 \t Column2\'.' ,
+																			'How do I enable interpretation of backslash escape sequences and print tab separated \'Column1 \t Column2\'?',
+																			'Print \'Column \t Column2\' after enabling interpretation of backslash escape sequences.']},ignore_index =  True)
+
+echo = echo.append({'Command':'echo -E \'Column1 \t Column2\'','NL Queries':['Disable interpretation of backslash escape sequences and print \'Column1 \t Column2\'.' ,
+																			'How do I disable interpretation of backslash escape sequences and print \'Column1 \t Column2\'?',
+																			'Print \'Column \t Column2\' after disabling interpretation of backslash escape sequences.']},ignore_index =  True)
+
+echo = echo.append({'Command':'echo $X','NL Queries':['Print value of variable X.',
+														'What is the value of shell variable x?',
+														'How do I output the value of variable x?']},ignore_index = True)
+
+echo = echo.append({'Command':'echo *','NL Queries':['Print all file names in current folder.',
+													'What files are present in this folder?',
+													'How do I find out the files present in this folder?',
+													'List all files in this folder.']},ignore_index = True)
+
+
+echo = echo.append({'Command':'echo -n \'This is echo command\'','NL Queries':['Print string \'This is echo command\' without a trailing newline.',
+																				'How do I print \'This is echo command\' without a new line at the end of the command output.',
+																				'Display string \'This is echo command\' on command line without a newline inserted at the end of the output.']},ignore_index = True)
+
+echo = echo.append({'Command':'echo -n -e \'A \v B\'','NL Queries':['Enable interpretation of backslash escape sequences and print \'A \v B\'. Do not insert newline after the output.' ,
+																'How do I enable interpretation of backslash escape sequences and print tab separated \'A \v B\' without a newline at the end of the command?',
+																'Print \'A \v B\' after enabling interpretation of backslash escape sequences. Do not add new line at the end of output.']},ignore_index =  True)
+
+echo = echo.append({'Command':'echo -E \'Firstname \\ Lastname\'','NL Queries':['Disable interpretation of backslash escape sequences and print \'Firstname \\ Lastname\'. Do not insert newline after the output.' ,
+																			'How do I disable interpretation of backslash escape sequences and print \'Firstname \\ Lastname\' without a newline at the end of the command??',
+																			'Print \'Firstname \\ Lastname\' after disabling interpretation of backslash escape sequences. Do not add new line at the end of output.']},ignore_index =  True)
+
+print echo.shape
