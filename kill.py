ecaa115f8dc5bebd03cc48eec0fa665b59b968db
Ananya Raval <ananya.raval@gmail.com>
2017-08-31 05:45:39 -0400

    5 commands

--- kill.py
+++ kill.py
@@ -2,8 +2,26 @@ import pandas as pd
 
 kill = pd.DataFrame(columns = ['Command','NL Queries'])
 
-kill = kill.append({'Command':'kill -l','NL Queries':['List signal names for kill.','Show all types of ways to terminate a perocess.']},ignore_index=True)
+kill = kill.append({'Command':'kill -l','NL Queries':['List signal names for kill.',
+													'Show all types of ways to terminate a process.',
+													'Display a list of signlas which can be sent to the currently executing processes of the system.']},ignore_index=True)
 
-print kill
+kill = kill.append({'Command':'kill 485','NL Queries':['Terminate process with PID 485.',
+														'How do I end process having PID 485?',
+														'Command to make process 485 stop executing.']},ignore_index=True)
+
+kill = kill.append({'Command':'kill -l 11','NL Queries':[' Show the signal number 11 to it\'s name.',
+														'What is the  name of signal 11?',
+														'Display the string name of signal number 11.']},ignore_index=True)
+
+kill = kill.append({'Command':'kill 123 543 2341 3453','NL Queries':['Kill all processes with ids 123 543 2341 3453.',
+																	'Terminate all processes with kill 123 543 2341 3453.',
+																	'How do I end processses 123 543 2341 3453?']},ignore_index=True)
+
+kill = kill.append({'Command':'kill SIGTERM 986','NL Queries':['Send SIGTERM to process 986.',
+																'How do I send the SIGTERM singla to the process with PID 986.',
+																'Command to send signal SIGTERM to currently executing process 986.']},ignore_index=True)
+
+print kill.shape
 
 
