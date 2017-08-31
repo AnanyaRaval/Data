ee617b5f2043426c1562cc347a1ed3c5251ff4c9
Ananya Raval <ananya.raval@gmail.com>
2017-08-31 05:59:56 -0400

    15 commands

--- less.py
+++ less.py
@@ -12,7 +12,6 @@ less = less.append({'Command':'less +4 instructions.txt','NL Queries':['Show con
 less = less.append({'Command':'less general.py ananya.py','NL Queries':['Show content of general.py and ananya.py.','Display lines of the files general.py and ananya.py.',
 						'Show contents of general.py and ananya.py.']},ignore_index=True)
 
-#17 July
 less = less.append({'Command':'less -a new.txt','NL Queries':['Open file with name new.txt. Start forward search from bottom of screen and backward search from top of screen.',
 						'Open file new.txt to search for a pattern in the file skipping all lines displayed on the screen.',
 							'How to search in lines of the file new.txt not displayed on screen ,while using less?',
@@ -50,12 +49,18 @@ less = less.append({'Command':'less --QUIT-AT-EOF hi.txt','NL Queries':['Open fi
 									'Display contents of hi.txt using less command. Exit once end of file has reached.',
 									'Show contents of hi.txt present in the current folder. Close file once end of file has reached.']},ignore_index = True)
 
-#--- Add commands for -g, -G, -h, -i, -I, -j, -t, -T, -y. For -p, add about 10 queries with different patterns for searching. Also, add as many combinations of these as you can.
-less = less.append({'Command':'less -f dir1','NL Queries':['Open a directory "dir1" in less','How to force-open dir1 directory in less ?','Open non-regular file -> dir1 in less.']},ignore_index = True)
+#--- Add commands for -g, -G, -h, -i, -I, -j, -t, -T, -y. }
+less = less.append({'Command':'less -f dir1','NL Queries':['Open a directory "dir1" forcefully on command line.',
+															'How to force open dir1 directory on command line?',
+															'Open folder named dir1 forcefully using less.']},ignore_index = True)
 
-less = less.append({'Command':'less -F list.txt','NL Queries':['Only open list.txt in command line, if it is more than a page wide','Open list.txt with less.If lists.txt has less than one screen of data,quit it automatically.','Display a pageful of lists.txt, if the total content is more than one page.']},ignore_index = True)
+less = less.append({'Command':'less -F list.txt','NL Queries':['Only open list.txt in command line, if it is more than a page wide',
+																'Open list.txt with less.If lists.txt has less than one screen of data,quit it automatically.',
+																'Display a pageful of lists.txt, if the total content is more than one page.']},ignore_index = True)
 
-less = less.append({'Command':'less -g list.txt','NL Queries':['Open list.txt in command line,one pageful at a time.While searching, highlight the last locations where the query was found in that pageful.','Open list.txt in "less" mode.Highlight only the last locations that match the query.','How to highlight only the last matching items found while seaching the file = list.txt in command line.']},ignore_index = True)
+less = less.append({'Command':'less -g list.txt','NL Queries':['Open list.txt in command line,one pageful at a time.While searching, highlight the last locations where the query was found in that pageful.',
+																'Open list.txt in "less" mode.Highlight only the last locations that match the query.',
+																'How to highlight only the last matching items found while seaching the file = list.txt in command line.']},ignore_index = True)
 
 less = less.append({'Command':'less -G list.txt','NL Queries':['Open list.txt in command line,one pageful at a time.While searching, go to the location where the query is matched in that pageful.','Open list.txt in "less" mode. Display the location where the query was matched, without highlighting the query word.','How to supress highlighting, while searching the file => list.txt in command line.']},ignore_index = True)
 
